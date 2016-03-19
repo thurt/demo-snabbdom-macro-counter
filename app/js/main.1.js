38539f133f28d0298c26508e6da2207ef0558a75
Taylor Hurt <taylor.a.hurt@gmail.com>
2016-03-19 06:19:01 +0000

    fixed rendering bug at line 56 in main; added simple localStorage support

--- app/js/main.1.js
+++ app/js/main.1.js
@@ -9,54 +9,86 @@ const patch = snabbdom.init([ // Init patch function with choosen modules
 const h = require('snabbdom/h')
 const view = Symbol('view')
 
+//HELPERS/////////////////////////////////////////////////
+function create(constructor, data) {
+  var new_c = new constructor()
+  Object.assign(new_c, data)
+
+  new_c[view] = new_c.getView()
+
+  list.components.push(new_c)
+  list.updateView()
+}
 //COMPONENTS//////////////////////////////////////////////
+const ComponentDict = { List, Toolbar, Counter }
+
 function List() {
   this.components = []
 }
-List.prototype.getView = function() {
-  return h('div', this.components.map(c => c[view]))
+List.prototype = {
+  getView() {
+    return h('div', this.components.map(c => c[view]))
+  },
+  updateView() {
+    var new_v = this.getView()
+    patch(this[view], new_v)
+    this[view] = new_v
+    localStorage.setItem('list', JSON.stringify(this))
+  }
 }
 
-function Toolbar() {}
-Toolbar.prototype.getView = function() {
-  var newButton = h('button', {
-    on: { click: this.new }
-  }, 'New')
-  return h('div', [newButton])
+function Toolbar() { this._type = 'Toolbar' }
+Toolbar.prototype = {
+  getView() {
+    var newButton = h('button', {
+      on: { click: () => {
+        create(ComponentDict[this.newType], this.newData)
+      } }
+    }, 'New')
+    return h('div', [newButton])
+  }
 }
 
-function Counter() {}
+function Counter() { this._type = 'Counter' }
 Counter.prototype = {
   getView() {
     var plusButton = h('button', {
-      on: { click: this.plus }
+      on: { click: () => {
+        this.count += 1
+        this[view] = this.getView()
+
+        list.updateView()
+      } }
     }, '+')
     var minusButton = h('button', {
-      on: { click: this.minus }
+      on: { click: () => {
+        this.count -= 1
+        this[view] = this.getView()
+
+        list.updateView()
+      } }
     }, '-')
     var countDiv = h('div', `Count: ${ this.count }`)
+
     return h('div', [plusButton, minusButton, countDiv])
   }
 }
-////////////////////////////////////////////////////////
 
+//SETUP//////////////////////////////////////////////////////////
 var list = new List()
-var toolbar = Object.assign(new Toolbar(), {
-  new() {
-    var counter = Object.assign(new Counter(), {
-      count: 0,
-      plus: function() {},
-      minus: function() {}
-    })
-
-    list.components.push(counter)
-
-    counter[view] = counter.getView()
-
-    patch(list[view], list.getView())
-  }
-})
-
-list.components.push(toolbar)
-toolbar[view] = toolbar.getView()
 patch(document.getElementById('list'), list[view] = list.getView())
+
+var list_data = JSON.parse(localStorage.getItem('list'))
+
+if (!list_data) {
+  create(Toolbar, { newType: 'Counter', newData: { count: 0 } })
+} else {
+  list_data.components.forEach(data => {
+    create(ComponentDict[data._type], data)
+  })
+}
+
+
+
+
+
